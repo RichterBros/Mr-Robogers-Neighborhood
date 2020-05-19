46cdcffba11be2bb0765fb5423aa898dd200a415
Erich Richter <erich.richter@gmail.com>
2020-05-18 22:26:02 -0700

    add js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,40 @@
 $(document).ready(function(){
+    number.numberArray();
+    });
+    
+    
+    
+    
+    function Numbers(userInput){
+       this.userInput = userInput;
+    }
+        
+    var number = new Numbers()
+        
+    
+    Numbers.prototype.numberArray = function() {
+        userInput =50;
+        newArr = []
+        newString = ""
+        
+        for(i=0; i < userInput + 1; i++){
+            newArr.push(i)
+           
+           }
+           
+           newString = newArr.join().split(',') 
+          
+           newString.forEach(function(){
+            
 
-});
-
-
-
+           });
+           
+           console.log(newString)
+            
+            
+           
+           
+        
+        
 
 
