4df2056c63e937e8ade71c5d3dcb35b3e9ceede5
Erich Richter <erich.richter@gmail.com>
2020-05-19 00:14:26 -0700

    complete business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,36 @@
 $(document).ready(function(){
     number.numberArray();
-    });
+});
     
     
     
     
-    function Numbers(userInput){
-       this.userInput = userInput;
+function Numbers(userInput, newArr, newString ){
+    this.userInput = userInput;
+    this.newArr = newArr;
+    this.newString = newString;
+}
+        
+var number = new Numbers(50,[],"")
+     
+Numbers.prototype.numberArray = function() {
+        
+    for(i=0; i < this.userInput + 1; i++){
+        this.newArr.push(i)
     }
-        
-    var number = new Numbers()
-        
     
-    Numbers.prototype.numberArray = function() {
-        userInput =50;
-        newArr = []
-        newString = ""
-        
-        for(i=0; i < userInput + 1; i++){
-            newArr.push(i)
-           
-           }
-           
-           newString = newArr.join().split(',') 
+    this.newString = this.newArr.join().split(',') 
           
-           for (i = 0; i < newString.length; i ++){
-            if (newString[i].includes("1") ){
-                newString[i] = "beep"
-            }
-
-        }
-           
-           console.log(newString)
-            
-        }
-           
-           
-        
-        
-
+    for (i = 0; i < this.newString.length; i ++){
+    if (this.newString[i].includes("3") ){
+        this.newString[i] = "Won't you be my neighbor?"
+    }else if (this.newString[i].includes("2") ){
+        this.newString[i] = "boop!"
+    }else if (this.newString[i].includes("1") ){
+        this.newString[i] = "beep!"
+        } 
+    }   
+    console.log(this.newString)    
+   
+}
 
