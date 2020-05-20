d5685c9cceb4f27101e2503fc9af47387b5b5870
Erich Richter <erich.richter@gmail.com>
2020-05-19 20:14:04 -0700

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,52 @@
-$(document).ready(function(){
-    
-    $("#submit").click(function() {
-        
-        number.numberArray();
-        
-    });  
+$(document).ready(function () {
 
+    $("#reset").click(function () {
+        location.reload();
+    });
+
+    $("#submit").click(function () {
+        number.numberArray();
+        $( "#roboRogers").show();
+        
+    });
 });
-    
-    
-    
-    
-function Numbers(userInput, newArr, newString){
+
+function Numbers(userInput, newArr, newString) {
     this.userInput = userInput;
     this.newArr = newArr;
     this.newString = newString;
+    this.counter = 0;
 }
-        
-var number = new Numbers(0, [],"")
-     
 
-Numbers.prototype.numberArray = function() {
-      
+var number = new Numbers(0, [], "")
+
+
+Numbers.prototype.numberArray = function () {
+    this.counter += 1;
     this.userInput = parseInt($("#userInput").val());
 
 
-    for(i=0; i < this.userInput + 1; i++){
+    for (i = 0; i < this.userInput + 1; i++) {
         this.newArr.push(i)
     }
+
+    this.newString = this.newArr.join().split(',')
+
+    for (i = 0; i < this.newString.length; i++) {
+        if (this.newString[i].includes("3")) {
+            this.newString[i] = "Won't you be my neighbor?"
+        } else if (this.newString[i].includes("2")) {
+            this.newString[i] = "boop!"
+        } else if (this.newString[i].includes("1")) {
+            this.newString[i] = "beep!"
+        } else {
+            $("#results").text("Please enter a number")
+        }
+    }
     
-    this.newString = this.newArr.join().split(',') 
-          
-    for (i = 0; i < this.newString.length; i ++){
-    if (this.newString[i].includes("3") ){
-        this.newString[i] = "Won't you be my neighbor?"
-    }else if (this.newString[i].includes("2") ){
-        this.newString[i] = "boop!"
-    }else if (this.newString[i].includes("1") ){
-        this.newString[i] = "beep!"
-        } 
-    }   
+    console.log(this.counter)
     console.log(this.userInput)
-    console.log(this.newString)    
+    console.log(this.newString)
     $("#results").text(this.newString)
-   
 }
 
