befc89c6bb8cdab2a16b437db337505028e4161d
Erich Richter <erich.richter@gmail.com>
2020-05-19 23:48:34 -0700

    add js functionality, css and html revisions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
-
+    
     $("#reset").click(function () {
         location.reload();
+       
+       
+    
     });
 
     $("#submit").click(function () {
         number.numberArray();
-        $( "#roboRogers").show();
-        
     });
+    
 });
 
 function Numbers(userInput, newArr, newString) {
@@ -25,28 +27,54 @@ Numbers.prototype.numberArray = function () {
     this.counter += 1;
     this.userInput = parseInt($("#userInput").val());
 
+    if (isNaN(this.userInput)) {
+        console.log("not a number")
+        $("#results").text("Please enter a number")
+    } else {
 
-    for (i = 0; i < this.userInput + 1; i++) {
-        this.newArr.push(i)
-    }
-
-    this.newString = this.newArr.join().split(',')
-
-    for (i = 0; i < this.newString.length; i++) {
-        if (this.newString[i].includes("3")) {
-            this.newString[i] = "Won't you be my neighbor?"
-        } else if (this.newString[i].includes("2")) {
-            this.newString[i] = "boop!"
-        } else if (this.newString[i].includes("1")) {
-            this.newString[i] = "beep!"
-        } else {
-            $("#results").text("Please enter a number")
+        if (this.counter === 1){
+            $("#roboRogers").show();
+            $("#roboRogers2").hide();
+            $("#roboRogers3").hide();
+        }else if (this.counter === 2){
+            $("#roboRogers").hide();
+            $("#roboRogers2").show();
+            $("#roboRogers3").hide();
+        }else if (this.counter === 3){
+            $("#roboRogers").hide();
+            $("#roboRogers2").hide();
+            $("#roboRogers3").show();
+            this.counter = 0;
         }
+            
+        
+
+        for (i = 0; i < this.userInput + 1; i++) {
+            this.newArr.push(i)
+        }
+
+        this.newString = this.newArr.join().split(',')
+
+        for (i = 0; i < this.newString.length; i++) {
+            if (this.newString[i].includes("3")) {
+                this.newString[i] = "Won't you be my neighbor?"
+            } else if (this.newString[i].includes("2")) {
+                this.newString[i] = "boop!"
+            } else if (this.newString[i].includes("1")) {
+                this.newString[i] = "beep!"
+            }
+        }
+
+
+
+        console.log(this.counter)
+        console.log(this.userInput)
+        console.log(this.newString)
+        $("#results").text(this.newString)
+        this.newArr = []
     }
-    
-    console.log(this.counter)
-    console.log(this.userInput)
-    console.log(this.newString)
-    $("#results").text(this.newString)
+
 }
+
+
 
